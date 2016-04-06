c81eabc7366aaf76aa959fa27055a6d79b5fccd0
buschbapti <baptiste.busch@inria.fr>
2016-04-06 15:04:06 +0200

    change ik testing

--- tests/test_compute_ik.py
+++ tests/test_compute_ik.py
@@ -2,6 +2,7 @@
 import unittest
 import rospy
 import time
+import numpy as np
 from human_moveit_config.human_model import HumanModel
 
 
@@ -10,91 +11,38 @@ class TestTransformation(unittest.TestCase):
         rospy.init_node('test_compute_ik')
         human = HumanModel()
 
-        start_head_joints = [0., 0., 0., 0., 0., 0.]
-        sides = ['right', 'left']
-        start_arm_joints = [0., 0., 0., 0., 0., 0., 0.]
-
         # move back to initial pose
-        human.move_group_by_joints('head', start_head_joints)
-        for j in range(2):
-            human.move_group_by_joints(sides[j]+'_arm', start_arm_joints)
+        init_state = human.get_current_state()
+        human.send_state(init_state)
 
         start = time.time()
         nb_iter = 0
+
+        # sides = ['right', 'left']
+        # links = ['torso', 'head_tip']
+        # for j in range(2):
+        #         links += [sides[j]+'_upper_arm', sides[j]+'_forearm', sides[j]+'_hand_tip']
+
+        links = ['right_hand_tip']
+
         while nb_iter < 100 and not rospy.is_shutdown():
             # get random pose
-            head_joint_values = human.get_random_joint_values('head')
-            # move the torso
-            human.move_group_by_joints('head', head_joint_values)
+            state = human.get_random_state()
 
-            # get joint values for the arm
-            arm_joint_values = []
-            for j in range(2):
-                joints = human.get_random_joint_values(sides[j]+'_arm')
-                human.move_group_by_joints(sides[j]+'_arm', joints)
-                arm_joint_values.append(joints)
+            fk = human.forward_kinematic(state, links=links)
 
-            # move back to initial pose
-            human.move_group_by_joints('head', start_head_joints)
-            for j in range(2):
-                human.move_group_by_joints(sides[j]+'_arm', start_arm_joints)
+            # calculate the inverse kinematic
+            ik = human.inverse_kinematic(fk)
+            # calculate fk(ik)
+            fk_compared = human.forward_kinematic(ik, links=links)
 
-            fk_head = human.forward_kinematic('head', head_joint_values, links=['torso', 'head_tip'])
-            # calculate ik for head
-            ik_torso = human.inverse_kinematic('head', [fk_head[0]], tolerance=0.001, links=['torso'])
-            # move to desired pose
-            human.move_group_by_joints('head', ik_torso)
-            # calculate head ik
-            ik_head = human.inverse_kinematic('head', [fk_head[1]], tolerance=0.001, links=['head_tip'])
-            # move to desired pose
-            human.move_group_by_joints('head', ik_head)
-
-            # calculate arm poses and move the arms
-            for j in range(2):
-                links = [sides[j]+'_upper_arm', sides[j]+'_forearm', sides[j]+'_hand_tip']
-                fk = human.forward_kinematic(sides[j]+'_arm', arm_joint_values[j], links=links)
-                for k in range(len(fk)):
-                    ik = human.inverse_kinematic(sides[j]+'_arm', [fk[k]], tolerance=0.001, links=[links[k]])
-                    # move to desired pose
-                    human.move_group_by_joints(sides[j]+'_arm', ik)
-
-            # get the joint values
-            current_head_joints = human.get_joint_values('head')
-            # head_ik = human.forward_kinematic('head', current_head_joints)
-            current_arm_joints = []
-            # arm_ik = []
-            for j in range(2):
-                current_arm_joints.append(human.get_joint_values(sides[j]+'_arm'))
-                # arm_ik.append(human.forward_kinematic(sides[j]+'_arm', current_arm_joints[j]))
-
-            print 'head'
-            print current_head_joints
-            print head_joint_values
-            print '-----------'
-            print 'right arm'
-            print current_arm_joints[0]
-            print arm_joint_values[0]
-            print '-----------'
-            print 'left arm'
-            print current_arm_joints[1]
-            print arm_joint_values[1]
+            print 'result'
+            for key, value in fk.iteritems():
+                print key + ': ' + str(value) + ' ~ ' + str(fk_compared[key])
+                print '---'
             print '-----------'
 
-            # assert equality between initial joint values and ik
-            # np.testing.assert_almost_equal(head_ik[0], head_des_pose[0], decimal=3)
-            # np.testing.assert_almost_equal(np.absolute(head_ik[1]), np.absolute(head_des_pose[1]), decimal=3)
-
-            # np.testing.assert_almost_equal(arm_ik[0][0], des_poses['right_hand_tip'][0], decimal=3)
-            # np.testing.assert_almost_equal(np.absolute(arm_ik[0][1]), np.absolute(des_poses['right_hand_tip'][1]), decimal=3)
-
-            # np.testing.assert_almost_equal(arm_ik[1][0], des_poses['left_hand_tip'][0], decimal=3)
-            # np.testing.assert_almost_equal(np.absolute(arm_ik[1][1]), np.absolute(des_poses['left_hand_tip'][1]), decimal=3)
-
-            # move back to initial pose
-            human.move_group_by_joints('head', start_head_joints)
-            for j in range(2):
-                human.move_group_by_joints(sides[j]+'_arm', start_arm_joints)
-
+            rospy.sleep(10)
             print 'run number '+str(nb_iter)+' passed'
             nb_iter += 1
             print "time: "+str(time.time() - start)
