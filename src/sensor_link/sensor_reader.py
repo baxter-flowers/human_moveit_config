6b87f933c53657f177433c2093d82eb12da49fe5
buschbapti <baptiste.busch@inria.fr>
2016-04-12 12:31:54 +0200

    Update the human model to be closer to kinect skeleton
    - Rename multiple links to avoid confusion between joints and links
    - Change base frame to base instead of hip
    - Add one more DOF in the back via spine joint
    - Change lengths of multiple segments

--- src/sensor_link/sensor_reader.py
+++ src/sensor_link/sensor_reader.py
@@ -23,14 +23,14 @@ class SensorReader(object):
         self.init_sensor_frames()
 
     def init_sensor_frames(self):
-        self.sensor_frames['kinect'] = ['head_tip', 'neck', 'torso', 'waist',
-                                        'left_shoulder', 'left_elbow', 'left_wrist', 'left_hand_tip',
-                                        'right_shoulder', 'right_elbow', 'right_wrist', 'right_hand_tip',
-                                        'left_hip', 'left_knee', 'left_ankle', 'left_foot_tip',
-                                        'right_hip', 'right_knee', 'right_ankle', 'right_foot_tip']
-        self.sensor_frames['optitrack'] = ['head_tip', 'torso', 'waist',
-                                           'left_shoulder', 'left_elbow', 'left_hand_tip',
-                                           'right_shoulder', 'right_elbow', 'right_hand_tip']
+        self.sensor_frames['kinect'] = ['head', 'neck', 'shoulder_center', 'spine', 'waist',
+                                        'left_shoulder', 'left_elbow', 'left_wrist', 'left_hand',
+                                        'right_shoulder', 'right_elbow', 'right_wrist', 'right_hand',
+                                        'left_hip', 'left_knee', 'left_ankle', 'left_foot',
+                                        'right_hip', 'right_knee', 'right_ankle', 'right_foot']
+        self.sensor_frames['optitrack'] = ['head', 'spine', 'waist',
+                                           'left_shoulder', 'left_elbow', 'left_hand',
+                                           'right_shoulder', 'right_elbow', 'right_hand']
 
     def copy_skeleton(self, sensor_key):
         for key, value in self.skel_data.iteritems():
@@ -62,47 +62,47 @@ class SensorReader(object):
     def generate_model_from_kinect(self):
         kinect_data = self.skeletons['kinect']
         # extract frame positions
+        p_waist = np.array(kinect_data['waist'][0])
+        p_spine = np.array(kinect_data['spine'][0])
+        p_neck = np.array(kinect_data['neck'][0])
+        p_head = np.array(kinect_data['head'][0])
+        p_shoulder_center = np.array(kinect_data['shoulder_center'][0])
+        p_shoulder = np.array(kinect_data['right_shoulder'][0])
         p_elbow = np.array(kinect_data['right_elbow'][0])
         p_wrist = np.array(kinect_data['right_wrist'][0])
-        p_neck = np.array(kinect_data['neck'][0])
-        p_torso = np.array(kinect_data['torso'][0])
-        p_head = np.array(kinect_data['head_tip'][0])
+        p_hip = np.array(kinect_data['right_hip'][0])
         p_knee = np.array(kinect_data['right_knee'][0])
         p_ankle = np.array(kinect_data['right_ankle'][0])
-        p_r_shoulder = np.array(kinect_data['right_shoulder'][0])
-        p_l_shoulder = np.array(kinect_data['left_shoulder'][0])
-        p_r_hip = np.array(kinect_data['right_hip'][0])
-        p_l_hip = np.array(kinect_data['left_hip'][0])
-        p_mid_shoulder = (p_r_shoulder - p_l_shoulder)/2
-        p_mid_hip = (p_r_hip - p_l_hip)/2
         # calculate the length of the model
-        upper_arm_l = float(np.linalg.norm(p_elbow - p_r_shoulder))
+        upper_arm_l = float(np.linalg.norm(p_elbow - p_shoulder))
         forearm_l = float(np.linalg.norm(p_wrist - p_elbow))
-        torso_l = float(np.linalg.norm(p_neck - p_torso))
-        waist_l = float(np.linalg.norm(p_torso))
+        torso_l = float(np.linalg.norm(p_neck - p_shoulder_center))
+        spine_up_l = float(np.linalg.norm(p_shoulder_center - p_spine))
+        spine_down_l = float(np.linalg.norm(p_spine - p_waist))
+        waist_l = float(np.linalg.norm(p_waist))
         neck_l = float(np.linalg.norm(p_head - p_neck))
-        thigh_l = float(np.linalg.norm(p_knee - p_r_hip))
+        thigh_l = float(np.linalg.norm(p_knee - p_hip))
         shin_l = float(np.linalg.norm(p_ankle - p_knee))
-        shoulder_offset_h = float(np.linalg.norm(p_mid_shoulder - p_torso))
-        shoulder_offset_w = float(np.linalg.norm(p_r_shoulder - p_mid_shoulder))
-        hip_offset_h = float(-np.linalg.norm(p_mid_hip))
-        hip_offset_w = float(np.linalg.norm(p_r_hip - p_mid_hip))
+        shoulder_offset = float(np.linalg.norm(p_shoulder - p_shoulder_center))
+        hip_offset = float(np.linalg.norm(p_hip))
         # set the disct of lengths for the urdf model
         self.lengths = {'upper_arm_length': upper_arm_l,
                         'forearm_length': forearm_l,
                         'torso_length': torso_l,
+                        'spine_up_length': spine_up_l,
+                        'spine_down_length': spine_down_l,
                         'waist_length': waist_l,
                         'neck_length': neck_l,
                         'thigh_length': thigh_l,
                         'shin_length': shin_l,
-                        'shoulder_offset_width': shoulder_offset_w,
-                        'shoulder_offset_height': shoulder_offset_h,
-                        'hip_offset_width': hip_offset_w,
-                        'hip_offset_height': hip_offset_h,
+                        'shoulder_offset_width': shoulder_offset,
+                        'hip_offset_width': hip_offset,
                         'hand_length': 0.1,    # TODO
                         'foot_length': 0.1,    # TODO
-                        'waist_radius': 0.12,  # TODO
-                        'torso_radius': 0.15,  # TODO
+                        'waist_radius': shoulder_offset-0.04,  # TODO
+                        'spine_radius': shoulder_offset-0.02,  # TODO
+                        'torso_radius': shoulder_offset,  # TODO
+                        'neck_radius': 0.04,  # TODO
                         'head_radius': 0.1}    # TODO
 
         print self.lengths
@@ -113,7 +113,7 @@ class SensorReader(object):
 
     def update_skeleton(self, sensors=None):
         def update_frame(target, prefix):
-            base = 'waist'
+            base = 'base'
             # loop through all the prefixes
             for b_pref in prefix:
                 for t_pref in prefix:
@@ -137,18 +137,18 @@ class SensorReader(object):
 
         def update_base_frame(prefix):
             for pref in prefix:
-                if self.tfl.canTransform(pref+'_frame', pref+'/human/waist', rospy.Time(0)):
-                    time = self.tfl.getLatestCommonTime('kinect_frame', pref+'/human/waist')
+                if self.tfl.canTransform(pref+'_frame', pref+'/human/base', rospy.Time(0)):
+                    time = self.tfl.getLatestCommonTime('kinect_frame', pref+'/human/base')
                     if rospy.Time.now() - time < rospy.Duration(0.5):
                         frame = self.tfl.lookupTransform('/'+pref+'_frame',
-                                                         '/'+pref+'/human/waist',
+                                                         '/'+pref+'/human/base',
                                                          time)
-                        self.skel_data['waist'] = [pref, frame]
+                        self.skel_data['base'] = [pref, frame]
                         # multiply with the calibration
                         if self.calibrated:
                             dot_prod = transformations.multiply_transform(frame,
-                                                                          self.calibration[pref]['waist'])
-                            self.skel_data['waist'] = [pref, dot_prod]
+                                                                          self.calibration[pref]['base'])
+                            self.skel_data['base'] = [pref, dot_prod]
                         return True
             return False
 
@@ -169,8 +169,7 @@ class SensorReader(object):
         list_frames = list(set(list_frames))
         # loop through all the possible frames
         for frame in list_frames:
-            if frame is not 'waist':
-                visible = (visible and update_frame(frame, prefix=sensors))
-            else:
-                visible = (visible and update_base_frame(prefix=sensors))
+            visible = (visible and update_frame(frame, prefix=sensors))
+        # update base pose
+        visible = (visible and update_base_frame(prefix=sensors))
         return visible
